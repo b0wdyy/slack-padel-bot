dac9868146deefc7fb034c81a0352f6871b9c3bc
b0wdyy <vandaelbodhi@hotmail.com>
2021-10-06 10:27:23 +0200

    :sparkles: feat: send statuscode

--- routes/hooks.routes.js
+++ routes/hooks.routes.js
@@ -1,31 +1,31 @@
-const { Router } = require('express');
-const axios = require('axios');
-const moment = require('moment');
-const { initCrawler, getDateFromDay, getLink } = require('../utils');
+const { Router } = require("express");
+const axios = require("axios");
+const moment = require("moment");
+const { initCrawler, getDateFromDay, getLink } = require("../utils");
 
 const router = Router();
 const days = [
-  'maandag',
-  'dinsdag',
-  'woensdag',
-  'donderdag',
-  'vrijdag',
-  'zaterdag',
-  'zondag',
+  "maandag",
+  "dinsdag",
+  "woensdag",
+  "donderdag",
+  "vrijdag",
+  "zaterdag",
+  "zondag",
 ];
 
-router.post('/padel', async (req, res) => {
+router.post("/padel", async (req, res) => {
   const { response_url, text, user_id } = req.body;
 
   if (!days.includes(text.toLowerCase())) {
-    return res.send('Geen geldige dag gegeven, makker...');
+    return res.send("Geen geldige dag gegeven, makker...");
   }
 
   const day = getDateFromDay(text);
-  const date = moment().day(day).format('DD-MM-YYYY');
+  const date = moment().day(day).format("DD-MM-YYYY");
 
   await axios.post(response_url, {
-    text: 'Even kijken hoeveel velden er nog vrij zijn...',
+    text: "Even kijken hoeveel velden er nog vrij zijn...",
   });
 
   const link = getLink({ date });
@@ -33,10 +33,16 @@ router.post('/padel', async (req, res) => {
   const actualFields = fields.length / 3;
 
   if (actualFields > 0) {
-    return res.send(`Er zijn nog ${actualFields} beschikbaar <@${user_id}>!! Boek nu: ${link} 🎾🎾`);
+    return res
+      .status(200)
+      .send(
+        `Er zijn nog ${actualFields} beschikbaar <@${user_id}>!! Boek nu: ${link} 🎾🎾`
+      );
   }
 
-  return res.send(`Slecht nieuws makker, geen velden meer vrij op ${text}... 😭😭`);
+  return res
+    .status(200)
+    .send(`Slecht nieuws makker, geen velden meer vrij op ${text}... 😭😭`);
 });
 
 module.exports = router;
--- utils/index.js
+++ utils/index.js
@@ -1,50 +1,44 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 function getDateFromDay(day) {
   switch (day) {
-    case 'maandag':
-      return 'Monday';
-    case 'dinsdag':
-      return 'Tuesday';
-    case 'woensdag':
-      return 'Wednesday';
-    case 'donderdag':
-      return 'Thursday';
-    case 'vrijdag':
-      return 'Friday';
-    case 'zaterdag':
-      return 'Zaterdag';
-    case 'zondag':
-      return 'Sunday';
+    case "maandag":
+      return "Monday";
+    case "dinsdag":
+      return "Tuesday";
+    case "woensdag":
+      return "Wednesday";
+    case "donderdag":
+      return "Thursday";
+    case "vrijdag":
+      return "Friday";
+    case "zaterdag":
+      return "Zaterdag";
+    case "zondag":
+      return "Sunday";
 
     default:
-      return 'No valid date given';
+      return "No valid date given";
   }
 }
 
 function getLink({ date }) {
   return `https://www.tennisvlaanderen.be/dagplanning?clubId=2388&planningDay=${date}&terrainGroupId=9245`;
 }
+
 async function initCrawler({ link }) {
   const LINK = link;
 
-  const browser = await puppeteer.launch({
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-    ],
-  });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(LINK);
   try {
     const fields = await page.$$eval(
-      '.reservation-table .Padel.available',
-      (data) => data.map(
-        (d) => d.getAttribute('data-url'),
-      ),
+      ".reservation-table .Padel.available",
+      (data) => data.map((d) => d.getAttribute("data-url"))
     );
 
-    return fields.filter((d) => d.includes('startHour=17:00'));
+    return fields.filter((d) => d.includes("startHour=17:00"));
   } catch (e) {
     return [];
   }
